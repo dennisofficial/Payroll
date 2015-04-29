a06a9b417fc3667518297e3ccbedf3374bcad4b4
Dennis Lysenko <dennislysenko@hotmail.com>
2015-04-29 17:12:16 -0400

    Created Command Type

---
+++ src/me/dennislysenko/payroll/type/Command.java
@@ -0,0 +1,23 @@
+package me.dennislysenko.payroll.type;
+
+public abstract class Command {
+
+	public String LABEL;
+	public String USAGE;
+	public String DESCRIPTION;
+	
+	public abstract void execute();
+
+	public String getLabel() {
+		return LABEL;
+	}
+	
+	public String getUsage() {
+		return USAGE;
+	}
+	
+	public String getDescription() {
+		return DESCRIPTION;
+	}
+	
+}
