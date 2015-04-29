43b86758c0eebb7d943fe262b38b10186a09a18e
Dennis Lysenko <dennislysenko@hotmail.com>
2015-04-29 17:11:13 -0400

    Added Command Register

--- src/me/dennislysenko/payroll/core/ThreadMain.java
+++ src/me/dennislysenko/payroll/core/ThreadMain.java
@@ -1,12 +1,13 @@
 package me.dennislysenko.payroll.core;
 
-
+import me.dennislysenko.payroll.engine.CommandManager;
 
 public class ThreadMain implements Runnable {
 
 	@Override
 	public void run() {
 		System.out.println(Main.TITLE + " v" + Main.VERSION);
+		CommandManager.registerCommands();
 	}
 
 }
