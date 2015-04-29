07c00631fd053597956deb897db2f0ae66d3e80f
Dennis Lysenko <dennislysenko@hotmail.com>
2015-04-29 17:10:58 -0400

    Created and Programmed Input Thread

---
+++ src/me/dennislysenko/payroll/core/ThreadInput.java
@@ -0,0 +1,37 @@
+package me.dennislysenko.payroll.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import me.dennislysenko.payroll.engine.CommandManager;
+
+public class ThreadInput implements Runnable {
+
+	private static Boolean running = true;
+	
+	@Override
+	public void run() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		while (running) {
+			try {
+				String line = reader.readLine();
+				if (line.startsWith("/")) {
+					line = line.substring(1);
+				}
+				CommandManager.execute(line);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void startThread() {
+		running = true;
+	}
+	
+	public static void stopThread() {
+		running = false;
+	}
+
+}
