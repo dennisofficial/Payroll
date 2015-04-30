dadb3c08290a3741d0aecab1f17820c901fc81fa
Dennis Lysenko <dennislysenko@hotmail.com>
2015-04-30 00:06:54 -0400

    Added Data Communicator API

---
+++ src/me/dennislysenko/payroll/api/DataWriter.java
@@ -0,0 +1,12 @@
+package me.dennislysenko.payroll.api;
+
+import me.dennislysenko.payroll.type.Client;
+import me.dennislysenko.payroll.type.PutAction;
+
+public class DataWriter {
+
+	public static void addData(Client building, PutAction action, String reference) {
+		
+	}
+	
+}
