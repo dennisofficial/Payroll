521fc7ad16cd41056b751d33051d72954368a5a2
Dennis Lysenko <dennislysenko@hotmail.com>
2015-05-01 00:40:44 -0400

    Added File Variable

--- src/me/dennislysenko/payroll/api/DataWriter.java
+++ src/me/dennislysenko/payroll/api/DataWriter.java
@@ -1,10 +1,15 @@
 package me.dennislysenko.payroll.api;
 
+import java.io.File;
+
+import me.dennislysenko.payroll.core.Main;
 import me.dennislysenko.payroll.type.Client;
 import me.dennislysenko.payroll.type.PutAction;
 
 public class DataWriter {
 
+	private static File data = new File(Main.getDataFolder(), "actions.dat");
+	
 	public static void addData(Client building, PutAction action, String reference) {
 		
 	}
