7e306355b9798b538bfc72be0c09379fca92550b
Dennis Lysenko <dennislysenko@hotmail.com>
2015-04-30 23:07:21 -0400

    Added Execute Method

--- src/me/dennislysenko/payroll/command/CmdPaycheck.java
+++ src/me/dennislysenko/payroll/command/CmdPaycheck.java
@@ -12,6 +12,17 @@ public class CmdPaycheck extends Command {
 	
 	@Override
 	public void execute(String[] args) {
+		Integer paid;
+		if (args.length != 1) {
+			System.out.println("Usage: " + USAGE);
+		}
+		else {
+			try {
+				paid = new Integer(args[0]);
+			} catch (NumberFormatException ex) {
+				System.out.println("Please enter a number only!");
+			}
+		}
 	}
 
 }
