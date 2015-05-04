7799b8b335fba2d5ca6ff9ea3a232d9c02982cf4
Dennis Lysenko <dennislysenko@hotmail.com>
2015-05-04 01:58:26 -0400

    Show Paycheck

--- src/me/dennislysenko/payroll/command/CmdPaycheck.java
+++ src/me/dennislysenko/payroll/command/CmdPaycheck.java
@@ -2,22 +2,33 @@ package me.dennislysenko.payroll.command;
 
 import me.dennislysenko.payroll.api.DataWriter;
 import me.dennislysenko.payroll.type.Command;
+import me.dennislysenko.payroll.type.Data;
+import me.dennislysenko.payroll.type.PutAction;
 
 public class CmdPaycheck extends Command {
 
 	public CmdPaycheck() {
 		LABEL = "PAYCHECK";
-		USAGE = "/paycheck <amount>";
+		USAGE = "/paycheck [amount]";
 		DESCRIPTION = "Adds a paycheck action.";
 	}
 	
 	@Override
 	public void execute(String[] args) {
 		Integer paid;
-		if (args.length != 1) {
-			System.out.println("Usage: " + USAGE);
+		if (args.length == 0 ) {
+			Integer paycheck = 0;
+			for (Data data : DataWriter.getData()) {
+				if (data.getAction().equals(PutAction.PAYCHECK)) {
+					paycheck -= data.getAmount();
+				}
+				else {
+					paycheck += data.getAmount();
+				}
+			}
+			System.out.println("Paycheck: $" + paycheck);
 		}
-		else {
+		else if (args.length == 1) {
 			try {
 				paid = new Integer(args[0]);
 				DataWriter.addData(paid);
@@ -25,6 +36,9 @@ public class CmdPaycheck extends Command {
 			} catch (NumberFormatException ex) {
 				System.out.println("Please enter a number only!");
 			}
+		}
+		else {
+			System.out.println("Usage: " + USAGE);
 		}
 	}
 
