4b493b3d249f9764172245a10846c80635ce24e3
Dennis Lysenko <dennislysenko@hotmail.com>
2015-05-04 00:14:29 -0400

    Added Amount, Adapted to Structure Change

--- src/me/dennislysenko/payroll/command/CmdPut.java
+++ src/me/dennislysenko/payroll/command/CmdPut.java
@@ -3,6 +3,7 @@ package me.dennislysenko.payroll.command;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Calendar;
 
 import me.dennislysenko.payroll.api.DataWriter;
 import me.dennislysenko.payroll.api.Table;
@@ -19,6 +20,7 @@ public class CmdPut extends Command {
 	private String action;
 	private String reference;
 	private Integer actionId;
+	private Integer amount;
 
 	public CmdPut() {
 		LABEL = "PUT";
@@ -34,7 +36,7 @@ public class CmdPut extends Command {
 
 			while (true) {
 				System.out.print("Building: ");
-				building = ClientManager.getClient(reader.readLine());
+				building = Client.getClient(reader.readLine());
 				if (building == null) {
 					System.out.println(building + " does not exist! Type: /client");
 					break;
@@ -62,7 +64,7 @@ public class CmdPut extends Command {
 					}
 					else if (action.equalsIgnoreCase(PutAction.QB.getLabel())) {
 						System.out.print("IN/PL: ");
-						reference = reader.readLine();
+						reference = reader.readLine().toUpperCase();
 						actionId = PutAction.QB.getID();
 						break;
 					}
@@ -70,16 +72,32 @@ public class CmdPut extends Command {
 						System.out.println("Action not found!");
 					}
 				}
-				String[] h = {"Date", "Building", "Reference", "Action"};
+				while (true) {
+					try {
+						System.out.print("Action Rate: ");
+						String temp = reader.readLine();
+						if (temp.isEmpty()) {
+							amount = 3;
+						}
+						else {
+							amount = new Integer(temp);
+						}
+						break;
+					} catch (NumberFormatException ex) {
+						System.out.println("Please enter a number!");
+					}
+				}
+				String[] h = {"Date", "Building", "Reference", "Action", "Amount"};
 				Table table = new Table(h);
-				TimeStamp time = new TimeStamp(TimeStamp.getTimeStamp());
-				table.addData(0, time.getMonth() + "/" + time.getDay());
+				Calendar cal = Calendar.getInstance();
+				table.addData(0, (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.DAY_OF_MONTH));
 				table.addData(1, building.getLabel());
 				table.addData(2, reference);
 				table.addData(3, PutAction.getAction(actionId).getLabel());
+				table.addData(4, amount.toString());
 				table.setMarginRight(1);
 				table.print();
-				DataWriter.addData(building, reference.replace(":", ";"), PutAction.getAction(action));
+				DataWriter.addData(building, reference.replace(":", ";"), PutAction.getAction(action), amount);
 				break;
 			}
 		} catch (IOException e) {
