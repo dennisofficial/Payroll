e6ed24e7861314d22d87b64474c1200914dda941
Dennis Lysenko <dennislysenko@hotmail.com>
2015-05-04 19:32:22 -0400

    Clear Screen on Startup

--- src/me/dennislysenko/payroll/core/Main.java
+++ src/me/dennislysenko/payroll/core/Main.java
@@ -2,6 +2,8 @@ package me.dennislysenko.payroll.core;
 
 import java.io.File;
 
+import me.dennislysenko.payroll.command.CmdClear;
+
 public class Main {
 
 	public static final Integer FPS = 20;
@@ -9,6 +11,7 @@ public class Main {
 	public static final String VERSION = "1.0.0";
 	
 	public static void main(String[] args) {
+		new CmdClear().execute(args);
 		new Thread(new ThreadMain()).start();
 		new Thread(new ThreadInput()).start();
 	}
